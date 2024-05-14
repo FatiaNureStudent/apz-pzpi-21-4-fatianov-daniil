b3e1f3c43f420ae37dc54f2a001c6bc266aea13a
Daniil Fatia <daniil.fatianov@nure.ua>
2024-05-14 22:21:44 +0300

    Created order-service app

---
+++ Task2/order-service/src/main/java/com/nure/apz/fatianov/daniil/orderservice/OrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nure.apz.fatianov.daniil.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+
+}
