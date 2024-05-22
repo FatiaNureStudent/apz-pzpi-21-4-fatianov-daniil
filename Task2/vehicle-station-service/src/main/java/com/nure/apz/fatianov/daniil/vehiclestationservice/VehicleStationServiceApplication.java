cb4df927f70c0b9e6432f99139226dfac7ceb48e
Daniil Fatia <daniil.fatianov@nure.ua>
2024-05-22 21:34:02 +0300

    Initial commit for vehicle-station-service

---
+++ Task2/vehicle-station-service/src/main/java/com/nure/apz/fatianov/daniil/vehiclestationservice/VehicleStationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nure.apz.fatianov.daniil.vehiclestationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleStationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleStationServiceApplication.class, args);
+	}
+
+}
